f529ab6224a91f97c367f0e4e84918a3db004c05
Jefta Dirksen <jeftadirksen@gmail.com>
2019-01-20 19:25:30 +0100

    Config

--- update.py
+++ update.py
@@ -2,15 +2,16 @@ import sys
 import os
 import urllib.request
 import json
-sys.path.insert(0, './MCRcon')
+sys.path.append('./MCRcon')
 import mcrcon
 
-# release or snapshot
-TYPE = "release"
+# Load config
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
 def main():
-    stop_server()
-    version = get_latest_version(TYPE)
+    stop_server(config["rcon_password"], config["rcon_host"], config["rcon_port"])
+    version = get_latest_version(config["type"])
     if file_exists(version["filename"]):
         print("No new version")
         os._exit(0)
@@ -45,9 +46,9 @@ def file_exists(filepath):
     """Test if the file exists."""
     return os.path.isfile(filepath)
 
-def stop_server(host="localhost", rcon_port=25575, rcon_password=""):
+def stop_server(host, rcon_port, rcon_password):
     rcon = mcrcon.MCRcon()
     rcon.connect(host, rcon_port, rcon_password)
-    rcon.command("list")
+    rcon.command("stop")
 
 main()
