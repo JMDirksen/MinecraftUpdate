660149770bf3402f08c3d63c171979d3e9fa1bb0
Jefta Dirksen <jeftadirksen@gmail.com>
2019-02-01 22:28:47 +0100

    PEP 8 stuff (PyCharm hints)

--- update.py
+++ update.py
@@ -1,14 +1,13 @@
-import sys
 import os
 import urllib.request
 import json
-sys.path.append('./MCRcon')
-import mcrcon
+from MCRcon.mcrcon import MCRcon
 from shutil import copyfile
 
 # Load config
 with open('config.json', 'r') as f:
     config = json.load(f)
+
 
 def main():
     version = get_latest_version(config["type"])
@@ -21,8 +20,9 @@ def main():
     try:
         copyfile(version["filename"], config["server_path"] + "server.jar")
         print("Update completed.")
-    except:
+    except FileNotFoundError:
         print("Unable to update server, is your config (server_path) correct?")
+
 
 def get_latest_version(type):
     """Get information about the latest version from Mojang."""
@@ -33,31 +33,36 @@ def get_latest_version(type):
         if latest_version["id"] == manifest["latest"][type]:
             break
     version = url_to_obj(latest_version["url"])
-    
+
     return {
         "id": version["id"],
         "filename": "minecraft_server.%s.jar" % version["id"],
         "download_url": version["downloads"]["server"]["url"],
-        }
+    }
+
 
 def url_to_obj(json_url):
     """Download the json and convert it to an object."""
     return json.load(urllib.request.urlopen(json_url))
 
+
 def download(url, filename):
     """Download a file and store it as filename."""
     urllib.request.urlretrieve(url, filename)
+
 
 def file_exists(filepath):
     """Test if the file exists."""
     return os.path.isfile(filepath)
 
+
 def stop_server(host, rcon_port, rcon_password):
     try:
-        rcon = mcrcon.MCRcon()
+        rcon = MCRcon()
         rcon.connect(host, rcon_port, rcon_password)
         rcon.command("stop")
-    except:
+    except ConnectionRefusedError:
         print("Failed to stop server, maybe it's not running? Or your config may be incorrect.")
+
 
 main()
